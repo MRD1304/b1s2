3b5bf63ba99967c647798b47526cb9a6f03f3c21
luarango <luisa.arango131@gmail.com>
2025-07-15 10:24:37 -0500

    ClaseEstadio

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,7 +3,18 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
-       
+       Estadio estadio1 = new  Estadio();
+       Estadio estadio2 = new Estadio();
+       estadio1.nombre = "Atanasio Girardot";
+       estadio2.ciudad = "Medellin";
+       estadio1.capacidad = 45000;
+       estadio2.nombre = "El Capin";
+       estadio2.ciudad = "Bogota";
+       estadio2.capacidad = 36000;
+       estadio1.mostrarInformacion();
+       estadio2.mostrarInformacion();
+
+
     }
 }
 
